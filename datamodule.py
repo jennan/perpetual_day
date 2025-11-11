30291d0000ea110807732d8eac8f7e2340b7588c
Maxime Rio <mr3857@gadi-gpu-v100-0123.gadi.nci.org.au>
2025-11-11 13:29:33 +1100

    add more scaffolding files

---
+++ datamodule.py
@@ -0,0 +1,80 @@
+from pathlib import Path
+from shutil import rmtree
+
+import numpy as np
+import torch
+import lightning as L
+from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
+from joblib import Parallel, delayed
+
+import pyearthtools.data as petdata
+
+
+class NpyDataset(Dataset):
+    def __init__(self, file_list):
+        self.file_list = file_list
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+        sample = np.load(self.file_list[idx])
+        return torch.from_numpy(sample).float()
+
+
+class PetDataModule(L.LightningDataModule):
+    def __init__(
+        self, pipeline, cache_dir, n_jobs, val_split: 0.1, test_split: 0.2, **kwargs
+    ):
+        super().__init__()
+        self.pipeline = pipeline
+        self.cache_dir = Path(cache_dir)
+        self.n_jobs = n_jobs
+        self.val_split = val_split
+        self.test_split = test_split
+        self.kwargs = kwargs
+        self.file_list = []
+
+    def _cache_data(self, date, i):
+        try:
+            sample = self.pipeline[date]
+            if not np.isnan(sample).any():
+                filename = self.cache_dir / f"sample_{str(date).replace(':', '')}.npy"
+                np.save(filename, sample)
+            else:
+                filename = None
+        except petdata.exceptions.DataNotFoundError:
+            filename = None
+        return filename
+
+    def prepare_data(self):
+        if self.cache_dir.is_dir():
+            rmtree(self.cache_dir)
+        self.cache_dir.mkdir(parents=True)
+        date_range = self.pipeline.iterator
+        file_list = Parallel(n_jobs=self.n_jobs, verbose=True)(
+            delayed(self._cache_data)(date, i) for i, date in enumerate(date_range)
+        )
+        self.file_list = sorted([fname for fname in file_list if fname])
+
+    def setup(self, stage: str):
+        train_files, test_files = train_test_split(
+            self.file_list, random_state=42, shuffle=True, test_size=self.test_split
+        )
+        train_files, val_files = train_test_split(
+            train_files, random_state=42, shuffle=True, test_size=self.val_split
+        )
+
+        self.test_ds = NpyDataset(test_files)
+        self.val_ds = NpyDataset(val_files)
+        self.train_ds = NpyDataset(train_files)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, shuffle=True, **self.kwargs)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, **self.kwargs)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_ds, **self.kwargs)
--- pipeline.py
+++ pipeline.py
@@ -1,33 +1,47 @@
+import pyearthtools.data as petdata
+import pyearthtools.pipeline as petpipe
+
+import site_archive_nci  # noqa
 
 
 def filter_days(iterator, bbox):
     # TODO implement filtering
-	return iterator
+    return iterator
 
 
 def features_pipeline(date_range, bbox):
+    """Himawari pipeline of the infrared bands"""
+    # TODO add normalisation
 
-	# filter days
+    # filter days
     valid_dates = filter_days(date_range, bbox)
 
-	# create pipeline
+    # create pipeline
     satproj = petdata.transforms.projection.HimawariProjAus()
-    sat_projector = petdata.transforms.projection.XYtoLonLatRectilinear(satproj)
-    # Get the Himawari pipeline of the infrared bands
-    input_bands = ['OBS_B08','OBS_B09','OBS_B10','OBS_B11','OBS_B12','OBS_B13','OBS_B14','OBS_B15','OBS_B16']
-    sat_accessor_IR = petdata.archive.HimawariChannels(bands=input_bands)
+    input_bands = [
+        "OBS_B08",
+        "OBS_B09",
+        "OBS_B10",
+        "OBS_B11",
+        "OBS_B12",
+        "OBS_B13",
+        "OBS_B14",
+        "OBS_B15",
+        "OBS_B16",
+    ]
+
     pipeline = petpipe.Pipeline(
-        sat_accessor_IR,
-        sat_projector,
-        petdata.transform.region.Bounding(-35, -25, 138, 150),  # cut down on region for example
-        petdata.transforms.variables.Drop(['x', 'y','geostationary']),
-        # petpipe.operations.xarray.normalisation.SingleValueDivision(1200),
+        petdata.archive.HimawariChannels(bands=input_bands),
+        petdata.transforms.projection.XYtoLonLatRectilinear(satproj),
+        petdata.transform.region.Bounding(*bbox),
+        petdata.transforms.variables.Drop(["x", "y", "geostationary"]),
         petpipe.operations.xarray.conversion.ToNumpy(),
         petpipe.operations.numpy.reshape.Squeeze(axis=1),
         iterator=valid_dates,
     )
-	return pipeline
+
+    return pipeline
+
 
 def target_pipeline(iterator, bbox):
-	pass
-
+    pass
