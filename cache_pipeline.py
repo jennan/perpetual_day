90adca1cc2d17853ee46efbd2d15b9dd0311d8f6
Maxime Rio <mr3857@gadi-gpu-v100-0112.gadi.nci.org.au>
2025-11-13 09:12:48 +1100

    increase amount of data cached

--- cache_pipeline.py
+++ cache_pipeline.py
@@ -10,7 +10,7 @@ from pipeline import full_pipeline
 
 if __name__ == "__main__":
     date_range = petpipe.iterators.DateRange(
-        "20200101T0000", "20200201T0000", interval="30 minutes"
+        "20210101T0030", "20220101T0000", interval="30 minutes"
     )
     bbox = [-35.27, -34, 150, 151.27]
     cachedir = "/scratch/nf33/mr3857/cache2"
