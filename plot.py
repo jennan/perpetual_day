4e597f288c9eb6a5934d6118b6f149fdefafc859
Yinjing Lin <yl4436@gadi-gpu-v100-0112.gadi.nci.org.au>
2025-11-12 14:31:20 +1100

    Update the plot script

--- plot.py
+++ plot.py
@@ -2,29 +2,9 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
-def plot(ax, proj, extent, lons, lats, data, title):
-    im = ax.pcolormesh(lons, lats, data, transform=proj, cmap='viridis')
-    ax.set_title(title)
+def plot_results(target, pred):
 
-    # Set extent for Australia [lon_min, lon_max, lat_min, lat_max]
-    ax.set_extent([110, 155, -45, -10], crs=proj)
-
-    # Add coastlines
-    ax.coastlines(resolution='10m')
-
-    # Add land and ocean features
-    ax.add_feature(cfeature.LAND, facecolor='lightgray')
-    ax.add_feature(cfeature.OCEAN, facecolor='lightblue')
-
-    # Add gridlines with labels
-    gl = ax.gridlines(draw_labels=True, linewidth=0.5, color='gray', alpha=0.5, linestyle='--')
-    gl.top_labels = False
-    gl.right_labels = False
-
-    ax.set_title(title)
-
-def plot_results(sample, pred, lons, lats):
-    
+    field = "channel_0003_scaled_radiance"
     # Define projection
     proj = ccrs.PlateCarree()
     extent = [110, 155, -45, -10]
@@ -32,15 +12,18 @@ def plot_results(sample, pred, lons, lats):
     # Create figure and axis
     fig, axes = plt.subplots(1, 3, figsize=(12, 5), subplot_kw={'projection': proj})
 
-    sample = sample[0].cpu().detach().numpy()
-    pred = pred[0].cpu().detach().numpy()
-    
-    lons_grid, lats_grid = np.meshgrid(lons,lats)
-    
-    # plot
-    plot(axes[0], proj, extent, lons_grid, lats_grid, sample, "Ground Truth")
-    plot(axes[1], proj, extent, lons_grid, lats_grid, pred, "Prediction")
-    plot(axes[2], proj, extent, lons_grid, lats_grid, pred - sample, "Diff")
+    target[field].plot(ax=axes[0], transform=proj)
+    pred[field].plot(ax=axes[1], transform=proj)
+    (pred - target)[field].plot(ax=axes[2], transform=proj)
+        
+    # Add coastlines and gridlines to each subplot
+    for ax in axes:
+        ax.coastlines(resolution='110m', color='black', linewidth=1)
+        
+        # Add gridlines
+        gl = ax.gridlines(draw_labels=True, linewidth=0.5, color='gray', alpha=0.5, linestyle='--')
+        gl.top_labels = False
+        gl.right_labels = False
     
     plt.tight_layout()
     plt.title('Perperual Day')
